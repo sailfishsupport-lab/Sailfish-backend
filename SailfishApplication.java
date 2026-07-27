f214f10071cd3d66b28b300d76a13d7cd76ea5f9
sailfishsupport-lab <sailfishsupport@gmail.com>
2026-07-28 00:08:21 +0530

    Add files via upload

---
+++ SailfishApplication.java
@@ -0,0 +1,11 @@
+package com.sailfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailfishApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SailfishApplication.class, args);
+    }
+}
