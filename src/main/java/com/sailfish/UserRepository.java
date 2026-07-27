6dd13e17c631efd355cb0fffc406da31b534ded6
sailfishsupport-lab <sailfishsupport@gmail.com>
2026-07-28 05:22:17 +0530

    Add findByEmail method to UserRepository
    Added method to find user by email in UserRepository.

--- src/main/java/com/sailfish/UserRepository.java
+++ src/main/java/com/sailfish/UserRepository.java
@@ -3,8 +3,10 @@ package com.sailfish;
 import com.sailfish.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    // Yeh MongoDB se apne aap data save aur fetch karne me madad karega
+    // Email ke zariye user ko dhoondne ke liye
+    Optional<User> findByEmail(String email);
 }
