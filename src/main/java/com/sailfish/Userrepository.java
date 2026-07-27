c18127ea44717174d69fc890eab85133ae2b929c
sailfishsupport-lab <sailfishsupport@gmail.com>
2026-07-28 05:10:43 +0530

    Update Userrepository.java

--- src/main/java/com/sailfish/Userrepository.java
+++ src/main/java/com/sailfish/Userrepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface Userrepository extends MongoRepository<User, String> {
     // Yeh MongoDB se apne aap data save aur fetch karne me madad karega
 }
