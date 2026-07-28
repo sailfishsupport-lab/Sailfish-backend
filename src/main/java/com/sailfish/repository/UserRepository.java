9bc27ae4e214d8dba7f6a731a70c3ae898b0c7ef
sailfishsupport-lab <sailfishsupport@gmail.com>
2026-07-28 21:07:52 +0530

    Update UserRepository.java

--- src/main/java/com/sailfish/repository/UserRepository.java
+++ src/main/java/com/sailfish/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.sailfish.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@repository
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByUsername(String username);
