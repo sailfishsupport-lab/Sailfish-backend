2e0ba7e71af1f2455fcc6146811ec9288604c973
sailfishsupport-lab <sailfishsupport@gmail.com>
2026-07-29 13:16:25 +0530

    Update UserController.java

--- src/main/java/com/sailfish/UserController.java
+++ src/main/java/com/sailfish/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
 public class UserController {
 
     @Autowired
